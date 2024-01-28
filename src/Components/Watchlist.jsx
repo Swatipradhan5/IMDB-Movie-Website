2200a49d265d26df59b596c6256b2744de53ae8e
Swati Pradhan <pradhanswati1997@gmail.com>
2024-01-28 10:48:04 +0530

    created and structured watchlist UI

--- src/Components/Watchlist.jsx
+++ src/Components/Watchlist.jsx
@@ -1,8 +1,349 @@
 function Watchlist(){
 
+    let movies = [
+        {
+            "adult": false,
+            "backdrop_path": "/jXJxMcVoEuXzym3vFnjqDW4ifo6.jpg",
+            "id": 572802,
+            "title": "Aquaman and the Lost Kingdom",
+            "original_language": "en",
+            "original_title": "Aquaman and the Lost Kingdom",
+            "overview": "Black Manta, still driven by the need to avenge his father's death and wielding the power of the mythic Black Trident, will stop at nothing to take Aquaman down once and for all. To defeat him, Aquaman must turn to his imprisoned brother Orm, the former King of Atlantis, to forge an unlikely alliance in order to save the world from irreversible destruction.",
+            "poster_path": "/7lTnXOy0iNtBAdRP3TZvaKJ77F6.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            28,
+            12,
+            14
+            ],
+            "popularity": 4197.714,
+            "release_date": "2023-12-20",
+            "video": false,
+            "vote_average": 6.87,
+            "vote_count": 862
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/2J5oxo9hNELC7GkUGpm6kByQi44.jpg",
+            "id": 933131,
+            "title": "Badland Hunters",
+            "original_language": "ko",
+            "original_title": "황야",
+            "overview": "After a deadly earthquake turns Seoul into a lawless badland, a fearless huntsman springs into action to rescue a teenager abducted by a mad doctor.",
+            "poster_path": "/zVMyvNowgbsBAL6O6esWfRpAcOb.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            28,
+            878
+            ],
+            "popularity": 115.508,
+            "release_date": "2024-01-25",
+            "video": false,
+            "vote_average": 6.8,
+            "vote_count": 20
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/ehumsuIBbgAe1hg343oszCLrAfI.jpg",
+            "id": 1022796,
+            "title": "Wish",
+            "original_language": "en",
+            "original_title": "Wish",
+            "overview": "Asha, a sharp-witted idealist, makes a wish so powerful that it is answered by a cosmic force – a little ball of boundless energy called Star. Together, Asha and Star confront a most formidable foe - the ruler of Rosas, King Magnifico - to save her community and prove that when the will of one courageous human connects with the magic of the stars, wondrous things can happen.",
+            "poster_path": "/AcoVfiv1rrWOmAdpnAMnM56ki19.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            16,
+            10751,
+            14,
+            12
+            ],
+            "popularity": 1176.469,
+            "release_date": "2023-11-13",
+            "video": false,
+            "vote_average": 6.506,
+            "vote_count": 391
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/yOm993lsJyPmBodlYjgpPwBjXP9.jpg",
+            "id": 787699,
+            "title": "Wonka",
+            "original_language": "en",
+            "original_title": "Wonka",
+            "overview": "Willy Wonka – chock-full of ideas and determined to change the world one delectable bite at a time – is proof that the best things in life begin with a dream, and if you’re lucky enough to meet Willy Wonka, anything is possible.",
+            "poster_path": "/qhb1qOilapbapxWQn9jtRCMwXJF.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            35,
+            10751,
+            14
+            ],
+            "popularity": 3210.944,
+            "release_date": "2023-12-06",
+            "video": false,
+            "vote_average": 7.186,
+            "vote_count": 1456
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/meyhnvssZOPPjud4F1CjOb4snET.jpg",
+            "id": 940551,
+            "title": "Migration",
+            "original_language": "en",
+            "original_title": "Migration",
+            "overview": "After a migrating duck family alights on their pond with thrilling tales of far-flung places, the Mallard family embarks on a family road trip, from New England, to New York City, to tropical Jamaica.",
+            "poster_path": "/ldfCF9RhR40mppkzmftxapaHeTo.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            16,
+            12,
+            35,
+            10751,
+            28
+            ],
+            "popularity": 1655.989,
+            "release_date": "2023-12-06",
+            "video": false,
+            "vote_average": 7.847,
+            "vote_count": 229
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/4W11akSpZBp8O09cQFEO0lqmVJl.jpg",
+            "id": 365620,
+            "title": "Ferrari",
+            "original_language": "en",
+            "original_title": "Ferrari",
+            "overview": "Set during the summer of 1957. Ex-racecar driver, Enzo Ferrari, is in crisis. Bankruptcy stalks the company he and his wife, Laura, built from nothing ten years earlier. Their tempestuous marriage struggles with the mourning for one son and the acknowledgement of another.",
+            "poster_path": "/xBFyJHvXCTgf9Vmq5uCnXRHrMQ4.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            18,
+            36
+            ],
+            "popularity": 190.604,
+            "release_date": "2023-12-14",
+            "video": false,
+            "vote_average": 6.744,
+            "vote_count": 231
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/dT1bXXEd7cfuVsB7CMvOKYiRZWl.jpg",
+            "id": 1007826,
+            "title": "The Underdoggs",
+            "original_language": "en",
+            "original_title": "The Underdoggs",
+            "overview": "Jaycen 'Two Js' Jennings is a washed-up former pro football star who has hit rock bottom. When he is sentenced to do community service coaching the Underdoggs, an unruly pee-wee football team in his hometown, he sees it mostly as an opportunity to rebuild his public image. But in the process, he may just turn his life around and rediscover his love of the game.",
+            "poster_path": "/wj1VRcVsh39nkxLkZP8tuGd96tc.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            35
+            ],
+            "popularity": 48.713,
+            "release_date": "2024-01-25",
+            "video": false,
+            "vote_average": 5.8,
+            "vote_count": 4
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/klb3yODwKMRle19EcvdeSatEZL7.jpg",
+            "id": 1072342,
+            "title": "Night Swim",
+            "original_language": "en",
+            "original_title": "Night Swim",
+            "overview": "Forced into early retirement by a degenerative illness, former baseball player Ray Waller moves into a new house with his wife and two children. He hopes that the backyard swimming pool will be fun for the kids and provide physical therapy for himself. However, a dark secret from the home's past soon unleashes a malevolent force that drags the family into the depths of inescapable terror.",
+            "poster_path": "/crqpDWr8yQiy7Xvj9GMgkmSrvE7.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            27,
+            53
+            ],
+            "popularity": 395.711,
+            "release_date": "2024-01-03",
+            "video": false,
+            "vote_average": 5.733,
+            "vote_count": 116
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/lVoHnqwfkDv4GHktEewBguhiNRn.jpg",
+            "id": 1183905,
+            "title": "Trunk: Locked In",
+            "original_language": "de",
+            "original_title": "Trunk - Locked In",
+            "overview": "Malina wakes up disoriented in the trunk of a speeding car and discovers to her horror that she is missing more than her memory. With her mobile phone as the only link to the outside world, she wages a desperate battle for survival.",
+            "poster_path": "/5KYOsr338jGBXfmdUT1prj4ZZW4.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            53,
+            28,
+            18
+            ],
+            "popularity": 7.175,
+            "release_date": "2023-11-24",
+            "video": false,
+            "vote_average": 6.2,
+            "vote_count": 4
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/fm6KqXpk3M2HVveHwCrBSSBaO0V.jpg",
+            "id": 872585,
+            "title": "Oppenheimer",
+            "original_language": "en",
+            "original_title": "Oppenheimer",
+            "overview": "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
+            "poster_path": "/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            18,
+            36
+            ],
+            "popularity": 536.381,
+            "release_date": "2023-07-19",
+            "video": false,
+            "vote_average": 8.117,
+            "vote_count": 6353
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/tLxjbT5ROZRwYcpNT3nfQbqkApk.jpg",
+            "id": 609681,
+            "title": "The Marvels",
+            "original_language": "en",
+            "original_title": "The Marvels",
+            "overview": "Carol Danvers, aka Captain Marvel, has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe. When her duties send her to an anomalous wormhole linked to a Kree revolutionary, her powers become entangled with that of Jersey City super-fan Kamala Khan, aka Ms. Marvel, and Carol’s estranged niece, now S.A.B.E.R. astronaut Captain Monica Rambeau. Together, this unlikely trio must team up and learn to work in concert to save the universe.",
+            "poster_path": "/9GBhzXMFjgcZ3FdR9w3bUMMTps5.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            878,
+            12,
+            28
+            ],
+            "popularity": 1807.709,
+            "release_date": "2023-11-08",
+            "video": false,
+            "vote_average": 6.367,
+            "vote_count": 1224
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/h0oBqUpax591vOacpBsDJ8cynjk.jpg",
+            "id": 792307,
+            "title": "Poor Things",
+            "original_language": "en",
+            "original_title": "Poor Things",
+            "overview": "Brought back to life by an unorthodox scientist, a young woman runs off with a debauched lawyer on a whirlwind adventure across the continents. Free from the prejudices of her times, she grows steadfast in her purpose to stand for equality and liberation.",
+            "poster_path": "/kCGlIMHnOm8JPXq3rXM6c5wMxcT.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            878,
+            10749,
+            35
+            ],
+            "popularity": 530.655,
+            "release_date": "2023-12-07",
+            "video": false,
+            "vote_average": 8.151,
+            "vote_count": 355
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/nvW0QfRJ7ga9TK8DKwBfAPA8ywP.jpg",
+            "id": 944401,
+            "title": "DogMan",
+            "original_language": "en",
+            "original_title": "Dogman",
+            "overview": "A boy, bruised by life, finds his salvation through the love of his dogs.",
+            "poster_path": "/xExDMuJHS0tqEfXGWUOR5rjP6xK.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            28,
+            18,
+            80
+            ],
+            "popularity": 154.143,
+            "release_date": "2023-09-27",
+            "video": false,
+            "vote_average": 7.4,
+            "vote_count": 209
+            },
+            {
+            "adult": false,
+            "backdrop_path": "/13QFKJa7ZPY3VN0L6sxKuNoLHFf.jpg",
+            "id": 781732,
+            "title": "Animal",
+            "original_language": "hi",
+            "original_title": "Animal",
+            "overview": "A son undergoes a remarkable transformation as the bond with his father begins to fracture, and he becomes consumed by a quest for vengeance.",
+            "poster_path": "/hr9rjR3J0xBBKmlJ4n3gHId9ccx.jpg",
+            "media_type": "movie",
+            "genre_ids": [
+            28,
+            80,
+            18
+            ],
+            "popularity": 60.946,
+            "release_date": "2023-12-01",
+            "video": false,
+            "vote_average": 7.2,
+            "vote_count": 56
+        }
+    ]
     return(
         <>
-        Watchlist
+            <div className="flex justify-center gap-2 my-3">
+                <span className="h-[3rem] w-[10rem] bg-blue-300 text-white text-center px-1 py-2
+                border rounded-full text-lg">
+                    All Genre
+                </span>
+            </div>
+            <div className="flex justify-center my-3">
+                <input type="text" placeholder="Search for a Movie" 
+                className="border border-cyan-400 rounded-full bg-slate-100 w-[20rem] h-[3rem] pl-10 outline-none"/>
+            </div>
+
+            <div className="border border-gray-400 rounded-md overflow-hidden shadow-md m-4">
+                <table className="w-full text-center">
+                    <thead className="bg-slate-200 h-[3rem] border-b-2 border-gray-300">
+                        <tr className="">
+                            <th className="text-left px-10">Name</th>
+                            <th className="flex gap-2 justify-center mt-2">
+                                <div className="bg-slate-100 border border-gray-300 rounded-full p-1 
+                                hover:cursor-pointer hover:scale-95">
+                                    <i class="fa-solid fa-chevron-up"></i>
+                                </div>
+                                <div>Ratings</div>
+                                <div className="bg-slate-100 border border-gray-300 rounded-full p-1 
+                                hover:cursor-pointer hover:scale-95">
+                                    <i class="fa-solid fa-chevron-down"></i>
+                                </div>
+                            </th>
+                            <th>Popularity</th>
+                            <th>Genre</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {movies.map((movieObj)=>{
+                            return <tr className="border-b-2">
+                                <td className="flex items-center px-4 m-3">
+                                    <img className="h-[5rem] w-[12rem]" src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`} alt="" />
+                                    <span className="ml-4">{movieObj.title}</span>
+                                </td>
+                                <td>{movieObj.vote_average}</td> 
+                                <td>{movieObj.popularity}</td>
+                                <td>Action</td>
+                                <td className="text-red-500">Remove</td>
+                            </tr>
+                            
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
